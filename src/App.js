b4374cb67e9e9135facb5ac3a4b787729de3d612
arashesmailian <arashesmailian@gmail.com>
2022-06-04 21:46:15 +0430

    pages implemented

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter , Routes , Route} from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Cart from './components/Cart';
 import Home from './components/Home';
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
